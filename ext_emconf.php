5624a209cc8a0ab3c8aeadaa9ad466939c8cbc09
Sebastian Stein <sebastian.stein@in2code.de>
2022-04-11 09:23:50 +0200

    [TASK] add TYPO3 11 to version constraints

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,13 +13,13 @@ $EM_CONF[$_EXTKEY] = [
     'dependencies' => 'extbase, fluid',
     'state' => 'stable',
     'author_company' => 'in2code GmbH',
-    'version' => '7.7.0',
+    'version' => '8.0.0-dev',
     'autoload' => [
         'psr-4' => ['In2code\\Migration\\' => 'Classes']
     ],
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-10.4.99'
+            'typo3' => '9.5.0-11.5.99'
         ],
         'conflicts' => [],
         'suggests' => [],
